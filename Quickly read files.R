e335eb3761f4e9b0f3e2cf2ae0fb148c82e9524b
ameliabridges <amelia.bridges@plymouth.ac.uk>
2023-09-21 21:36:51 +0100

    x

--- Quickly read files.R
+++ Quickly read files.R
@@ -30,10 +30,10 @@ my_plot_theme <- function(...){
                    ...)
 }
 
-slo<-raster("D:/OneDrive/OneDrive - University of Plymouth/In_prep_submittted_manuscripts/Howell_etal_Global_HC/Non-hierarchical_habitat_classification/InputLayers/Unprocessed/GEBCO2020_GoG_Slo_10km_WGS84.tif")
-bbpi<-raster("D:/OneDrive/OneDrive - University of Plymouth/In_prep_submittted_manuscripts/Howell_etal_Global_HC/Non-hierarchical_habitat_classification/InputLayers/Unprocessed/GEBCO2020_GoG_BBPI_10km_WGS84.tif")
-fbpi<-raster("D:/OneDrive/OneDrive - University of Plymouth/In_prep_submittted_manuscripts/Howell_etal_Global_HC/Non-hierarchical_habitat_classification/InputLayers/Unprocessed/GEBCO2020_GoG_FBPI_10km_WGS84.tif")
-prod<-raster("D:/OneDrive/OneDrive - University of Plymouth/In_prep_submittted_manuscripts/Howell_etal_Global_HC/Non-hierarchical_habitat_classification/InputLayers/Unprocessed/Yool_GoG_POC_10km_WGS84.tif")
-temp<-raster("D:/OneDrive/OneDrive - University of Plymouth/In_prep_submittted_manuscripts/Howell_etal_Global_HC/Non-hierarchical_habitat_classification/InputLayers/Unprocessed/BO_GoG_Temp_10km_WGS84.tif")
-sal<-raster("D:/OneDrive/OneDrive - University of Plymouth/In_prep_submittted_manuscripts/Howell_etal_Global_HC/Non-hierarchical_habitat_classification/InputLayers/Unprocessed/BO_GoG_Sal_10km_WGS84.tif")
-gog_shp<-terra::vect("D:/OneDrive/OneDrive - University of Plymouth/In_prep_submittted_manuscripts/Howell_etal_Global_HC/Non-hierarchical_habitat_classification/InputLayers/Gulf_of_Guinea.shp")
+slo<-raster(paste0(raw_input_layers_folder, "/GEBCO2020_GoG_Slo_10km_WGS84.tif"))
+bbpi<-raster(paste0(raw_input_layers_folder, "/GEBCO2020_GoG_BBPI_10km_WGS84.tif"))
+fbpi<-raster(paste0(raw_input_layers_folder, "/GEBCO2020_GoG_FBPI_10km_WGS84.tif"))
+prod<-raster(paste0(raw_input_layers_folder, "/Yool_GoG_POC_10km_WGS84.tif"))
+temp<-raster(paste0(raw_input_layers_folder, "/BO_GoG_Temp_10km_WGS84.tif"))
+sal<-raster(paste0(raw_input_layers_folder, "/BO_GoG_Sal_10km_WGS84.tif"))
+gog_shp<-terra::vect(paste0(clust_input_layers_folder, "/Gulf_of_Guinea.shp"))
